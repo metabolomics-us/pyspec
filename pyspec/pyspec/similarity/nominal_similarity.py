55606dc7cd502e042291b4e0818ac03bbb3f9d28
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2019-10-15 10:55:58 -0400

    added support for binned cosine similarity in nominal_similarity

--- pyspec/pyspec/similarity/nominal_similarity.py
+++ pyspec/pyspec/similarity/nominal_similarity.py
@@ -11,24 +11,41 @@ def _transform_spectrum_tuple(spectrum):
     return [x for x in s if x[1] > 0]
 
 
-def _transform_spectrum(spectrum, nominal_bin=True, normalize=True, scale_function=None):
-    if type(spectrum) == str:
-        spectrum = [x.split(':') for x in spectrum.split()]
+def _transform_spectrum(spectrum, bin_size=None, normalize=True, scale_function=None):
+    """
 
+    :param spectrum:
+    :param bin_size: bin width for describing sensitivity of spectrum similarity
+    :param normalize: whether to normalize the binned spectrum (base peak intensity is 100)
+    :param scale_function: custom function to transform the bin weights using mass and intensity
+    :return:
+    """
+
+    if type(spectrum) == str:
+        spectrum = _transform_spectrum_tuple(spectrum)
+
+    # build binned spectrum
     bins = collections.defaultdict(float)
 
     for mz, intensity in spectrum:
-        key = int(float(mz) + MZ_ROUND_CORRECTION) if nominal_bin else float(mz)
+        if bin_size is None:
+            # convert to nominal mass spectrum and include m/z correction factor
+            key = int(mz + MZ_ROUND_CORRECTION)
+        else:
+            # generate bin index if bin size is provided
+            key = int(mz / bin_size)
+
         bins[key] += float(intensity)
 
     max_intensity = max(bins.values())
 
+    # transform spectrum through normalization and/or custom scaling
     transformed_spectrum = collections.defaultdict(float)
 
     for k, v in bins.items():
         if v > 0:
             transformed_spectrum[k] = v
-             
+
             if normalize:
                 transformed_spectrum[k] = 100 * transformed_spectrum[k] / max_intensity
 
@@ -38,40 +55,62 @@ def _transform_spectrum(spectrum, nominal_bin=True, normalize=True, scale_functi
     return transformed_spectrum
 
 
-def cosine_similarity(a, b):
-    if type(a) in [str, list]:
-        a = _transform_spectrum(a)
-    if type(b) in [str, list]:
-        b = _transform_spectrum(b)
+def cosine_similarity(a, b, bin_size=None):
+    """
+    calculate the standard cosine similarity between two spectra
+    :param a:
+    :param b:
+    :param bin_size: allow for a custom bin size, otherwise transform to nominal mass
+    :return:
+    """
 
+    # handle different input formats
+    if type(a) in [str, list]:
+        a = _transform_spectrum(a, bin_size=bin_size)
+    if type(b) in [str, list]:
+        b = _transform_spectrum(b, bin_size=bin_size)
+
+    # calculate norm for each spectrum using all ions
     normA = sum(v * v for v in a.values())
     normB = sum(v * v for v in b.values())
 
+    # calculate cosine similarity
     if normA == 0 or normB == 0:
         return 0
+    else:
+        shared_ions = sorted(set(a.keys()) & set(b.keys()))
+        product = math.pow(sum(a[k] * b[k] for k in shared_ions), 2)
 
-    sharedIons = sorted(set(a.keys()) & set(b.keys()))
-    product = math.pow(sum(a[k] * b[k] for k in sharedIons), 2)
+        return product / normA / normB
 
-    return product / normA / normB
 
 def composite_similarity(a, b):
+    """
+    calculate composite similarity between two spectra
+    note: this is currently only defined for nominal mass spectra and cannot be used with custom binning
+    :param a:
+    :param b:
+    :return:
+    """
+
+    # handle different input formats
     if type(a) == str:
         a = _transform_spectrum(a)
     if type(b) == str:
         b = _transform_spectrum(b)
-    
-    sharedIons = sorted(x for x in set(a.keys()) & set(b.keys()) if a[x] > EPS_CORRECTION and b[x] > EPS_CORRECTION)
-    cosineSimilarity = cosine_similarity(a, b)
 
-    if len(sharedIons) > 1:
-        ratiosA = [a[x] / a[y] for x, y in zip(sharedIons, sharedIons[1:])]
-        ratiosB = [b[x] / b[y] for x, y in zip(sharedIons, sharedIons[1:])]
-        combinedRatios = [x / y for x, y in zip(ratiosA, ratiosB)]
+    # identify shared ions and calculate cosine similarity
+    shared_ions = sorted(x for x in set(a.keys()) & set(b.keys()) if a[x] > EPS_CORRECTION and b[x] > EPS_CORRECTION)
+    cosine_sim = cosine_similarity(a, b)
 
-        intensitySimilarity = 1 + sum(x if x < 1 else 1 / x for x in combinedRatios)
+    if len(shared_ions) > 1:
+        ratios_a = [a[x] / a[y] for x, y in zip(shared_ions, shared_ions[1:])]
+        ratios_b = [b[x] / b[y] for x, y in zip(shared_ions, shared_ions[1:])]
+        combined_ratios = [x / y for x, y in zip(ratios_a, ratios_b)]
 
-        return (len(a) * cosineSimilarity + intensitySimilarity) / (len(a) + len(sharedIons))
+        intensity_sim = 1 + sum(x if x < 1 else 1 / x for x in combined_ratios)
+
+        return (len(a) * cosine_sim + intensity_sim) / (len(a) + len(shared_ions))
 
     else:
-        return cosineSimilarity
+        return cosine_sim
