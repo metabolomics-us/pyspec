4677749898c619f4752092f593f7ff0196a4431b
berlinguyinca <berlinguyinca@gmail.com>
2020-05-25 13:40:23 -0700

    fixed file path issue

--- pyspec/tests/pyspec/converter/test_LCBinBaseToPostgresConverter.py
+++ pyspec/tests/pyspec/converter/test_LCBinBaseToPostgresConverter.py
@@ -5,6 +5,6 @@ from pyspec.converter.carrot_msms_dump_to_postgres import LCBinBaseToPostgresCon
 
 def test_convert_dump():
     converter = LCBinBaseToPostgresConverter()
-    count = converter.convert("datasets/B2b_SA1594_TEDDYLipids_Neg_MSMS_1U2WN.msms.json.gz", compressed=True)
+    count = converter.convert("../datasets/B2b_SA1594_TEDDYLipids_Neg_MSMS_1U2WN.msms.json.gz", compressed=True)
 
     assert count > 0
